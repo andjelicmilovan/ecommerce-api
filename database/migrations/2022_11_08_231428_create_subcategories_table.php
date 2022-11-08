6f134b806afedfa6d65da8328c66dbfd0cacc02b
Milovan Andjelic <milovan.andjelic@ict-investment.me>
2022-11-09 00:28:01 +0100

    Intial commit

---
+++ database/migrations/2022_11_08_231428_create_subcategories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->integer('category_id')->unsigned()->index();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategories');
+    }
+};
